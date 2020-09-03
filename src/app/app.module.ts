e4cc61d4c031289ed47c650af6b8e162c147dba5
anumathew19 <anumathew595@gmail.com>
2020-09-03 11:22:08 +0530

    third commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { StartAnglrComponent } from './start-anglr/start-anglr.component';
 import { ServerComponent } from './server/server.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StartAnglrComponent,
-    ServerComponent
+    ServerComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule,
