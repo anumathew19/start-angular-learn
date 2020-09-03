4068e00f0861de7f691d5d9789e94412537eeb95
anumathew19 <anumathew595@gmail.com>
2020-09-03 10:15:22 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { StartAnglrComponent } from './start-anglr/start-anglr.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartAnglrComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/start-anglr/start-anglr.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-start-anglr',
+  template: './start-anglr.component.html',
+  styleUrls: ['./start-anglr.component.css']
+})
+export class StartAnglrComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
